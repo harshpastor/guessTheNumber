2640a7c473c44308e38dfd1ba5bb5753fd88c522
Harsh Pastor <129552409+harshpastor@users.noreply.github.com>
2024-06-14 22:01:25 +0000

    added details

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+let i=10
+const arr=[]
+while(i>0){
+  let form = document.querySelector("form")
+  form.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    i--
+    let number=Math.floor((Math.random()*100))+1
+    let choi=form.guessField.value
+    let choice= parseInt(choi)
+    if(choice===number){
 
+    }
+    else{
+        arr.push(number)
+    }
+    let guess=document.querySelector(".guesses");
+    let guessed=""
+    for(let j=0;j<arr.length;i++){
+        guessed+=toString(arr[j])
+    }
+    guess.innerHTML=`<h3>${guessed}</h3>`
+    console.log(guessed)
+  })
+
+
+}
